ccfdd020d222e3975fc90ff55fa78905a1a07447
Carsten Behring <carsten.behring@gmail.com>
2024-09-12 09:32:14 +0200

    improved docu

--- src/msal_helper/msal.clj
+++ src/msal_helper/msal.clj
@@ -16,6 +16,20 @@
                       masl4j-token-cache-kv-secret-url
                       client-id-for-kv
                       client-secret-for-kv]
+  "
+  Returns two fns which know how to read/write a String (usualy serialized json) to a Azure Key Vault secret.
+  These we call `the cache`
+  
+Parameters are:
+  
+`masl4j-token-cache-kv-secret-url` : The concrete secret URL in an Azure Key Vault to write to in form of https://a-key-vault.vault.azure.net/secrets/a-secret-name
+`client-id-for-kv` : an app-id which has permissions to read and write the secret in `masl4j-token-cache-kv-secret-url`
+`client-secret-for-kv`  : app secret of `client-id-for-kv`
+`tenant-id` :  Tenant of the Key Vault and client-id used 
+  
+  
+  The returned cache and its fns are used by `device-code-interactive-login` and  `try-silent-authenticate` 
+  as data accessor in a ITokenCacheAccessAspect during authentication"
   {:get-data-fn (fn []
                   (let [access-token-for-key-vault (azure/do-client-credentials  tenant-id "https://vault.azure.net" client-id-for-kv client-secret-for-kv)]
                     (azure/get-secret-data masl4j-token-cache-kv-secret-url access-token-for-key-vault)))
@@ -44,15 +58,23 @@
         build)))
 
 (defn device-code-interactive-login
-  "Authenticates interactively via device code flow and populates the cache. (a given Azure Key Vault)
-  See function `try-silent-authenticate` for parameters.
+  "Authenticates interactively via device code flow and refreshes the cache. 
+
+   `tenant-id` :  Tenant of the client-id used for authentication
+   `client-id` : Client-id (or app-id) which will be used for authentication
+   `scope`     : Set of scopes the access token should allow and which the user will eventualy asked to consent to
+   `cache`     : A cache created by fn `azure-kv-cache`. It is used to read and write the auth cache data
+
+
   It returns a `java.util.concurrent.CompletableFuture` object which on de-referencing it
   will block and print on console the  code to use interaticely with Microsofts
   device code endpoint: https://microsoft.com/devicelogin
   The dereferenced value will be of type `com.microsoft.aad.adal4j.AuthenticationResult` and containing the access codes.
 
-  After sucessfull interactive authentication the token data will be cached
+  After sucessfull interactive authentication the token data will be cached freshly,
   and following calls to `try-silent-authenticate` work without user intercation.
+
+  See here: https://learn.microsoft.com/en-us/entra/msal/java/getting-started/device-code-flow
   "
   [tenant-id
    client-id
@@ -81,17 +103,29 @@
 (defn try-silent-authenticate
   "Tries to authenticate silently with Microsoft Identity without user interaction.
 
-  It uses the given Azure Key Vault Secret as token cache.
-  For this it reads token cache data from the specified Azure key vault secret `masl4j-token-cache-key-vault-secret-url`.
-  It uses `client-id-for-kv` and `client-secret-for-kv` to authenticate to the key vault, so the Azure application `client-id-for-kv`
-  needs to have adequate  permission on the key vault to read and write the secret specified by `masl4j-token-cache-key-vault-secret-url`
+  It uses the given cache (as created by fn azure-kv-cache)  as token cache.
+  It reads token cache data from the cache, which allows to silently authenticate again Azure AD,
+  given that the data in the cache is not expired.
 
-  The user specified by `user-name` need to be in the cached token data.
-  `tenant-id` needs to be the tenant of the user-name
-  `client-id` is the app to be authenticated for with the scope `scope`
+  `user-name` : Email of user to authenticate, which need to be in the cached token data.
+              An initial call to `device-code-interactive-login` makes this sure.
+  `tenant-id` : Tenant of the user and client-id use for authentication
+  `client-id` : Client-id (or app-id) which will be used for authentication
+  `scopes` : Set of scopes the access token should allow to use 
+  `cache` : A  cache created by fn `azure-kv-cache`. It is used to read and write the auth cache data
 
+  So using the function refreshes the cached authentication information inside the given cache as a side-effect
+  
   It returns on sucess an object of type `com.microsoft.aad.adal4j.AuthenticationResult`
   which contains the access token for the given user (if found in cached data)
+
+  The function can fail at any moment, when the cached data expired or the 
+  user did not yet consent to the scopes in `scopes`.
+  In this case the fn `device-code-interactive-login` can be used to refresh the cached data,
+  and to trigger re-consent if needed.
+  This which requires a user present and it will use the device login authentication.
+
+  If this functions is called regulary, the cache data will not expire.
   "
   [user-name
    tenant-id
