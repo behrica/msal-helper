618cd92567355657b0d391374c0b7eb464a8d3d0
Carsten Behring <carsten.behring@efsa.europa.eu>
2025-03-27 14:36:27 +0000

    added local cache

--- src/msal_helper/azure.clj
+++ src/msal_helper/azure.clj
@@ -4,19 +4,19 @@
    [clj-http.client :as client]))
 
 
-   
-(defn do-client-credentials[tenant-id resource client-id client-secret]
+
+(defn do-client-credentials [tenant-id resource client-id client-secret]
   (->
    (client/post (str "https://login.microsoftonline.com/" tenant-id  "/oauth2/token")
                 {:form-params {:resource  resource
                                :client_id client-id
                                :client_secret client-secret
                                :grant_type "client_credentials"}
-                               
+
                  :as :json})
-                
+
    (get-in [:body :access_token])))
-   
+
 
 
 (defn get-secret-data [secret-url access-token]
@@ -36,14 +36,14 @@
 
 
 
-(defn call-ms-graph[f suffix access-token]
+(defn call-ms-graph [f suffix access-token]
   (:body (f (str "https://graph.microsoft.com/v1.0/" suffix)
             {:headers {:Authorization (str "Bearer " access-token)
                        :Accept "application/json"}
 
              :as :json})))
-               
-              
+
+
 
 (defn me [access-token]
   (call-ms-graph client/get  "me" access-token))
@@ -58,6 +58,6 @@
                   :as :json
                   :content-type :json
                   :body (json/encode {:Message {:subject subject
-                                                :body  {  :contentType "Text"
+                                                :body  {:contentType "Text"
                                                         :content body-text}
-                                                :toRecipients [ {:emailAddress {:address to}}]}})})))
+                                                :toRecipients [{:emailAddress {:address to}}]}})})))
